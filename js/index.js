67af2ff46657b455349886761ea9be224a4e61bf
ExistedGit <66470490+ExistedGit@users.noreply.github.com>
2023-12-08 11:10:54 +0100

    + Bracket generation in JS

---
+++ js/index.js
@@ -0,0 +1,31 @@
+let obj = [
+	{
+		name: "round-one",
+		matchups: [
+			{
+				teams: ["ZSB-E", "ZSO"],
+				score: [1, 0],
+				date: "12-12-2019 12:00",
+			},
+			{
+				teams: ["Kek", "Hej"],
+				score: [0, 1],
+				date: "12-12-2019 12:00",
+			},
+		],
+	},
+];
+
+addEventListener("load", (ev) => {
+	for (const round of obj) {
+		let rounds = document.querySelectorAll(`.${round.name} .matchup`);
+
+		for (let i = 0; i < round.matchups.length; i++) {
+			const elem = rounds[i];
+			const matchup = round.matchups[i];
+			for (let j = 0; j < matchup.teams.length; j++) {
+				elem.innerHTML += `<li class="team">${matchup.teams[j]}<span class="score">${matchup.score[j]}</span></li>`;
+			}
+		}
+	}
+});
