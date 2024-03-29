812a7ef4c42c3ff0e1976d3f80a4efe7158dc455
ExistedGit <66470490+ExistedGit@users.noreply.github.com>
2024-03-29 20:30:38 +0100

    cringe

--- js/index.js
+++ js/index.js
@@ -1,84 +1,156 @@
-// There are 3 rounds defined, each of which has multiple matchups of 2 teams
-// You will need to fill in the teams and the score of the match into the respective fields
-// You should also add status: true if you are adding a new matchup so it's displayed as active
-// There is also an optional date parameter in case you want to show it
-// Inspired by https://codepen.io/jbeason/pen/Wbaedb mention on website https://blog.codepen.io/2018/02/16/need-make-tournament-bracket/ autor: Joe Beason
+// There are three rounds in total: eliminations, semifinals and finals
+// The first two are divided into three sectors, where 2 matchups of 2 teams each compete to form a single matchup with 2 teams
 let sectors = [
+	// Let this sector serve as an example
 	// Sector A
 	{
-		name: "SectorA",
 		first_matchups: [
 			{
-				team1: "K",
-				team2: "O",
-
-				active: true,
+				// Here there are two teams. Team 1 has a score of 0 and Team 2 has 2 points
+				members: {
+					"Team 1": 0,
+					"Team 2": 2,
+				},
+				winner: 2, // 1 = Team 1, 2 = Team 2 and so on
+				inactive: true, // Set to true if you haven't yet decided on the matchup so it stays greyed out
+				ongoing: false, // Set to true if it's currently played
 			},
 			{
-				team1: "K",
-				team2: "O",
+				members: {
+					"Team 3": 2,
+					"Team 4": 1,
+				},
+				winner: 1,
+				inactive: true,
+				ongoing: false,
 			},
 		],
 		second_matchup: {
-			team1: "",
-			team2: "",
+			members: {
+				"Team 2": 0,
+				"Team 3": 2,
+			},
+			winner: 2,
+			inactive: true,
+			ongoing: false,
 		},
 	},
 	// Sector B
 	{
 		first_matchups: [
 			{
-				team1: "K",
-				team2: "O",
+				members: {
+					"Team 1": 0,
+					"Team 2": 2,
+				},
+				winner: 1,
+				inactive: true,
+				ongoing: false,
 			},
 			{
-				team1: "K",
-				team2: "O",
+				members: {
+					"Team 1": 0,
+					"Team 2": 2,
+				},
+				winner: 1,
+				inactive: true,
+				ongoing: false,
 			},
 		],
 		second_matchup: {
-			team1: "K",
-			team2: "O",
+			members: {
+				"Team 1": 0,
+				"Team 2": 2,
+			},
+			winner: 1,
+			inactive: true,
+			ongoing: false,
 		},
 	},
 	// Sector C
 	{
 		first_matchups: [
 			{
-				team1: "K",
-				team2: "O",
+				members: {
+					"Team 1": 0,
+					"Team 2": 2,
+				},
+				winner: 1,
+				inactive: true,
+				ongoing: false,
 			},
 			{
-				team1: "K",
-				team2: "O",
+				members: {
+					"Team 1": 0,
+					"Team 2": 2,
+				},
+				winner: 1,
+				inactive: true,
+				ongoing: false,
 			},
 		],
-		second_mathchup: {
-			team1: "K",
-			team2: "O",
+		second_matchup: {
+			members: {
+				"Team 1": 0,
+				"Team 2": 2,
+			},
+			winner: 1,
+			inactive: true,
+			ongoing: false,
 		},
 	},
 ];
 
-// Sector D
-sector_d = {
-	team1: "K",
-	team2: "O",
-	team3: "L",
+let finals = {
+	members: {
+		"Team 1": 0,
+		"Team 2": 1,
+		"Team 3": 2,
+	},
+	winner: 2,
+	inactive: true,
+	ongoing: false,
 };
+
 // Change null to a string with the team's name
 // like
 // let champion = "ZSB-E";
 let champion = null;
 
-addEventListener("load", (ev) => {
-	for (const sector of sectors) {
-		for (let matchup of sector.first_matchups) {
-			// matchup
-		}
-		sector.second_matchup;
-	}
+function addMatchup(elem, matchup) {
+	if (matchup.inactive) {
+		elem.classList.add("inactive");
+	} else if (matchup.ongoing) elem.classList.add("ongoing");
 
+	for (const [teamName, score] of Object.entries(matchup.members)) {
+		elem.innerHTML += `<li class="team"><span>${teamName}</span><span class="score">${score}</span></li>`;
+	}
+}
+
+addEventListener("load", (ev) => {
+	let sectorElems = document.querySelectorAll(".sector"),
+		semisElems = document.querySelectorAll(".semis-section");
+	for (let i = 0; i < sectorElems.length; i++) {
+		let sector = sectorElems[i],
+			semis = semisElems[i];
+		let sectorObj = sectors[i],
+			sectorMatchupElems = sector.querySelectorAll(".matchup");
+
+		for (let j = 0; j < sectorObj.first_matchups.length; j++) {
+			let matchup = sectorObj.first_matchups[j],
+				matchupElem = sectorMatchupElems[j];
+			addMatchup(matchupElem, matchup);
+		}
+
+		let semisMatchupElem = semis.querySelector(".matchup"),
+			matchup = sectorObj.second_matchup;
+
+		addMatchup(semisMatchupElem, matchup);
+	}
+	addMatchup(
+		document.querySelector(".matchup-container-round-three .matchup"),
+		finals
+	);
 	// Adding matchups
 	// for (const round of obj) {
 	// 	let rounds = document.querySelectorAll(`.${round.name} .matchup`);
@@ -88,7 +160,7 @@ addEventListener("load", (ev) => {
 	// 		const matchup = round.matchups[i];
 	// 		for (let j = 0; j < matchup.teams.length; j++) {
 	// 			if (matchup.status !== undefined || matchup.status) {
-	// 				elem.innerHTML += `<li class="team current-team">${matchup.teams[j]}<span class="score">${matchup.score[j]}</span></li>`;
+	// 				elem.innerHTML += `<li class="team">${matchup.teams[j]}<span class="score">${matchup.score[j]}</span></li>`;
 	// 				elem.classList.add("active");
 	// 			} else {
 	// 				elem.innerHTML += `<li class="team">&nbsp;<span class="score"></span></li>`;
